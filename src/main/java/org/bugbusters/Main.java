5306d4cada0ff7f149d10c34d9c28f6e5fdfacea
IvanGonzaloGarciaAli1 <ivan.gonzalo.garcia.ali1@gmail.com>
2025-04-27 11:43:05 +0200

    Base Backend

--- src/main/java/org/bugbusters/Main.java
+++ src/main/java/org/bugbusters/Main.java
@@ -4,10 +4,6 @@ package org.bugbusters;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
+        System.out.println("Prueba de github");
     }
 }
