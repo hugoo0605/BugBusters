d76e64c1daf65b52261bd631d3d13e17aacea85e
hugoo0605 <hugoserratgarcia@gmail.com>
2025-05-06 13:23:06 +0200

    cambios main

--- src/main/java/org/bugbusters/Main.java
+++ src/main/java/org/bugbusters/Main.java
@@ -2,9 +2,13 @@ package org.bugbusters;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "org.bugbusters.repository")
+@EntityScan(basePackages = "org.bugbusters.model")
 public class Main {
     public static void main(String[] args) {
         System.out.println("Iniciando programa...");
