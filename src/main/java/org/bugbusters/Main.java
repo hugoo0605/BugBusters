71b436db6692aa8ce1a6392bcbf1dca6a2d00087
hugoo0605 <hugoserratgarcia@gmail.com>
2025-05-07 12:40:39 +0200

    de vuelta al programa normal

--- src/main/java/org/bugbusters/Main.java
+++ src/main/java/org/bugbusters/Main.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "org.bugbusters.repository")
 public class Main {
     public static void main(String[] args) {
         System.out.println("Iniciando programa...");
