8cc9d0986c205b5765b3c82220b503254bd2df25
hugoo0605 <hugoserratgarcia@gmail.com>
2025-05-15 00:34:57 +0200

    filtrado por categoria para la barra del menu

--- src/main/java/org/bugbusters/repository/ProductoRepository.java
+++ src/main/java/org/bugbusters/repository/ProductoRepository.java
@@ -2,11 +2,14 @@ package org.bugbusters.repository;
 
 import org.bugbusters.entity.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import java.util.List;
 
 public interface ProductoRepository extends JpaRepository<Producto, Long> {
     List<Producto> findByDisponibleTrue();
     List<Producto> findByCategoriaAndDisponibleTrue(String categoria);
     List<Producto> findByNombreContainingIgnoreCaseAndDisponibleTrue(String nombre);
-
+    @Query("SELECT DISTINCT p.categoria FROM Producto p")
+    List<String> findDistinctCategorias();
 }
